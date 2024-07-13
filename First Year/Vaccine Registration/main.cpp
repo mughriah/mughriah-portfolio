3dec305f58aa9745d400d8b12ee9cfd78d3b12fa
Mary Faye Caya <146728425+mughriah@users.noreply.github.com>
2024-07-13 20:59:48 +0800

    Create main.cpp

---
+++ First Year/Vaccine Registration/main.cpp
@@ -0,0 +1,33 @@
+
+/***********************************************************************************
+**********************
+This is to certify that this project is my own work, based on my personal efforts
+in studying and applying the concepts
+learned. I have constructed the functions and their respective algorithms and
+corresponding code by myself. The
+program was run, tested, and debugged by my own efforts. I further certify that I
+have not copied in part or whole or
+otherwise plagiarized the work of other students and/or persons.
+Joehanna L. Cansino, DLSU ID# 12112596
+Mary Faye Q. Caya, DLSU ID# 12141422
+***********************************************************************************
+**********************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX_USERS 100
+#include "Display Header.cpp"
+#include "Function.cpp"
+
+int main ()
+
+{
+	
+	int choice;
+	Menu(choice); // function call to display the main menu
+	return 0; // exit code of the program.
+
+}
+
+
